93f39b43ffa249827a3c8929633eab99fb9aa269
Kelvin Brenand <brenand.kelvin@gmail.com>
2020-04-18 03:41:05 -0300

    Create BJP4 Self-Check 5.15: isVowel.java

---
+++ Practice-it Exercises/BJP4 Self-Check 5.15: isVowel.java
@@ -0,0 +1,9 @@
+public static boolean isVowel(String s){
+        if (s.equalsIgnoreCase("A") || s.equalsIgnoreCase("E") ||
+                s.equalsIgnoreCase("I") || s.equalsIgnoreCase("O") ||
+                s.equalsIgnoreCase("U")){
+            return true;
+        }else{
+            return false;
+        }
+    }
